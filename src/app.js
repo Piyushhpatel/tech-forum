452d67c41377a31eb0986e50529590d9a5995b22
Piyush Patel <piyushpatel1746@gmail.com>
2024-10-06 17:22:28 +0530

    Attempt cors fix

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-}));
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+  
 
 app.use(express.json());
 
