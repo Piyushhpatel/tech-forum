3d08e7371aa205d135e4ce2d0a395dcb28e567eb
Piyush Patel <piyushpatel1746@gmail.com>
2024-10-06 17:40:02 +0530

    Attempt 4

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "*",
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     })
   );
   
