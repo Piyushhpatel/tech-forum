3b7e3015ba6613fa67000c774c8f2839a55f625d
Piyush Patel <piyushpatel1746@gmail.com>
2024-10-06 17:30:09 +0530

    Attempt 2 to fix cors

--- src/app.js
+++ src/app.js
@@ -3,17 +3,37 @@ import cors from "cors";
 const app = express();
 
 app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    })
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    process.env.CORS_ORIGIN
   );
-  
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Cracked Nerds Forum");
+  res.send("Welcome to the Cracked Nerds Forum");
 });
 
 //Routes
@@ -25,4 +45,4 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/comments", commentRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 
-export {app};
+export { app };
